7f8266c7b402ba8b16519db66ea5f772cfd56346
Thomas Czachorowski <Thomas.Czachorowski@cdpipelinedevshop.com>
2022-08-18 10:56:16 -0400

    Initial commit

---
+++ src/main/java/Input01.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+
+public class Input01 {
+    public static void main(String[] args) {
+        //Create a JOptionPane.
+        //Store the input as a String and print it.
+//String input = JOptionPane.showInputDialog("Type Something");
+
+
+        //Parse the input as an int.
+        //Print its value +1
+//int input2= Integer.parseInt(input);
+
+
+        //Try creating a dialog, parsing it, and initializing an int in a single line.
+        //You should have only one semicolon (;) in this line.
+        int input2= Integer.parseInt(JOptionPane.showInputDialog("Type Something"));
+
+        System.out.println(++input2);
+    }
+}
